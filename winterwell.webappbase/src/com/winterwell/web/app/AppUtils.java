2c0718f4b4eb7a1511f76ee97a3d1a3dd2052df3
Daniel Winterstein <daniel@good-loop.com>
2023-09-25 21:24:37 +0100

    clean-up-debug

--- winterwell.webappbase/src/com/winterwell/web/app/AppUtils.java
+++ winterwell.webappbase/src/com/winterwell/web/app/AppUtils.java
@@ -569,9 +569,6 @@ public class AppUtils {
 		ESHttpClient es = Dep.get(ESHttpClient.class);
 		String errMsg = null;			
 		for(Class k : dbclasses) {
-			if ("NGO".equals(k.getSimpleName())) {
-				System.out.println(k);
-			}
 			for(KStatus status : statuses) {
 				ESPath path = esRouter.getPath(dataspace, k, null, status);
 				try {					
