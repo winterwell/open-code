7002fef29b7995bd79b73f9b3ee5326d7e4b00a4
Daniel Winterstein <daniel@good-loop.com>
2023-10-24 14:33:13 +0100

    SLF4J-notes

---
+++ winterwell.webappbase/src/com/winterwell/web/app/Log2SLF4J.java
@@ -0,0 +1,19 @@
+//package com.winterwell.web.app;
+//
+//import com.winterwell.utils.log.ILogListener;
+//import com.winterwell.utils.log.Log;
+//import com.winterwell.utils.log.Report;
+// TODO if we want SLF4J logging - just uncomment this, add the jars, and make one of these in AMain.
+//public class Log2SLF4J implements ILogListener {
+//
+//	Logger logger = LoggerFactory.getLogger(Log2SLF4J.class);
+//
+//	public Log2SLF4J() {
+//		Log.addListener(this);		
+//	}
+//
+//	@Override
+//	public void listen(Report report) {
+//	    logger.info(report);
+//	}
+//}
