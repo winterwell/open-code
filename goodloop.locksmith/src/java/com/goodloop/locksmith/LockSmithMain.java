70958e879e911432be9534381f111e466203bd6c
Roscoe <roscoe@good-loop.com>
2023-11-10 15:03:06 +0000

    Locksmith: put YouAgainClient in Dep so it doesn't throw 500s on every call

--- goodloop.locksmith/src/java/com/goodloop/locksmith/LockSmithMain.java
+++ goodloop.locksmith/src/java/com/goodloop/locksmith/LockSmithMain.java
@@ -1,20 +1,11 @@
 package com.goodloop.locksmith;
 
-import java.util.Arrays;
-import java.util.Timer;
-
-import com.winterwell.datalog.DataLog;
-import com.winterwell.datalog.DataLogConfig;
-import com.winterwell.datalog.IDataLogAdmin;
+import com.goodloop.adserver.GLBaseConfig;
 import com.winterwell.utils.Dep;
-import com.winterwell.utils.log.Log;
-import com.winterwell.utils.log.LogFile;
-import com.winterwell.utils.time.TUnit;
 import com.winterwell.web.app.AMain;
-import com.winterwell.web.app.AppUtils;
 import com.winterwell.web.app.JettyLauncher;
-import com.winterwell.web.app.KServerType;
 import com.winterwell.web.app.MasterServlet;
+import com.winterwell.youagain.client.YouAgainClient;
 
 /**
  * Runs this for a standalone DataLog micro-service server.
@@ -33,6 +24,13 @@ public class LockSmithMain extends AMain<LockSmithConfig> {
 	public static void main(String[] args) {	
 		LockSmithMain dls = new LockSmithMain();
 		dls.doMain(args);
+		
+		// HACK 1: Magic string "good-loop" - rather than adding adserver
+		// to classpath just for GLBaseConfig.GOOD_LOOP_APPNAME
+		// HACK 2: product doesn't do anything yet, so this doesn't
+		// necessarily tie locksmith to portal
+		YouAgainClient yac = new YouAgainClient("good-loop", "portal.good-loop.com");
+		Dep.set(YouAgainClient.class, yac);
 	}
 	
 	@Override
