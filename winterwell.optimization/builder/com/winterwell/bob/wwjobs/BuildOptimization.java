8852a80a60ecc92710b4872f34d57d0408b36ce3
athomson0 <aidan@good-loop.com>
2024-01-18 16:11:46 +0000

    finished winterwell.* - re-add bob build configs to allow for interim release before full switchover

---
+++ winterwell.optimization/builder/com/winterwell/bob/wwjobs/BuildOptimization.java
@@ -0,0 +1,10 @@
+package com.winterwell.bob.wwjobs;
+
+public class BuildOptimization extends BuildWinterwellProject {
+
+	public BuildOptimization() {
+		super("winterwell.optimization");
+		setVersion("1.0.0"); // 22 Mar 2021
+	}
+
+}
---
+++ winterwell.web/builder/com/winterwell/bob/wwjobs/BuildWeb2.java
@@ -0,0 +1,73 @@
+package com.winterwell.bob.wwjobs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.winterwell.bob.BuildTask;
+import com.winterwell.bob.tasks.MavenDependencyTask;
+/**
+ * A copy of BuildWeb in the bob project itself. This can be used from the command line.
+ * @author daniel
+ *
+ */
+public class BuildWeb2 extends BuildWinterwellProject {
+
+
+	/**
+	 * Build winterwell.web
+	 */
+	public BuildWeb2() {
+		super("winterwell.web");
+		setVersion("1.1.4"); // 31 May 2022
+	}	
+
+	@Override
+	public List<BuildTask> getDependencies() {
+		ArrayList deps = new ArrayList(super.getDependencies());
+
+		// utils
+		deps.add(new BuildUtils());
+		
+		// maven
+		MavenDependencyTask mdt = new MavenDependencyTask();
+		// see https://mvnrepository.com/artifact/org.eclipse.jetty/jetty-server
+		String jettyVersion = 
+				"10.0.15"; // websockets
+//				"10.0.7"; // hopefully fix Ambiguous segment error for slugs with an encoded "/"
+		// NB: v11 is the same as v10 but switches s/javax/jakarta/
+//				"9.4.24.v20191120"; 
+		mdt.addDependency("org.eclipse.jetty", "jetty-server", jettyVersion);
+		mdt.addDependency("org.eclipse.jetty","jetty-util",jettyVersion);
+		mdt.addDependency("org.eclipse.jetty","jetty-util-ajax",jettyVersion);
+		mdt.addDependency("org.eclipse.jetty", "jetty-servlet",jettyVersion);
+		
+		mdt.addDependency("com.sun.mail", "jakarta.mail", "2.0.1"); //"1.5.0-b01");
+		mdt.addDependency("com.sun.mail", "gimap", "2.0.1"); // used??
+//		mdt.addDependency("jakarta.mail", "jakarta.mail-api", "1.6.1"); //"1.5.0-b01");
+//		mdt.addDependency("jakarta.mail", "imap", "1.4"); //"1.5.0-b01");
+		
+		mdt.addDependency("eu.medsea.mimeutil", "mime-util", "1.3"); // latest is "2.1.3" but that seems borked
+		mdt.addDependency("org.apache.httpcomponents", "httpclient", "4.5.10");
+		mdt.addDependency("org.ccil.cowan.tagsoup", "tagsoup", "1.2.1");
+		// Apache
+		mdt.addDependency("commons-fileupload","commons-fileupload","1.4");
+		mdt.addDependency("commons-io", "commons-io", "2.6");
+		// https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+		mdt.addDependency("org.apache.commons","commons-lang3","3.9");
+
+		// DNS Java -- NB: Latest is v3 Carson tried it in 2019, and it had oddly poor outputs! (worse than v2) 
+		// Surely it must be better if we have the time to work it out? OTOH v2 is fine and we don't care.
+		mdt.addDependency("dnsjava", "dnsjava", "2.1.9");
+		
+		mdt.setIncSrc(true); // we like source code
+//		mdt.setForceUpdate(true);
+		mdt.setProjectDir(projectDir);
+		if (outDir!=null) {
+			mdt.setOutputDirectory(outDir);
+		}
+		deps.add(mdt);
+		
+		return deps;
+	}
+	
+}
